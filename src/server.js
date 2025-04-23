f8794f66dd69e47f9bba231f526d397f10ed6168
quyenngogia <ngogiaquyendhtn223@gmail.com>
2025-04-24 03:43:21 +0700

    allow access for ngogiaquyen.id.vn

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ const io = new Server(server, {
       "http://localhost:5500",
       "http://127.0.0.1:3000",
       "http://localhost:3000",
+      "https://ngogiaquyen.id.vn/",
     ], // Khớp với cấu hình CORS
     methods: ["GET", "POST"],
   },

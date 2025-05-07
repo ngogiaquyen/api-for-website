9f30b6a7a77f147fcbe74428f2c9274618bbdfb9
quyenngogia <ngogiaquyendhtn223@gmail.com>
2025-05-08 00:50:15 +0700

    fix core

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const userRoutes = require("./modules/user/routes/user.route");
 const postRoutes = require("./modules/post/routes/post.route");
 const messageRoute = require("./modules/Message/routes/Message.route");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3000", "https://ngogiaquyen.id.vn"],
+    origin: [
+      "http://localhost:3000",
+      "https://ngogiaquyen.id.vn",
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
@@ -16,11 +20,8 @@ app.use(
 app.use(express.json());
 
 app.use("/users", userRoutes);
-
-// post api blog web
 app.options("/api/posts", cors());
 app.use("/api/posts", postRoutes);
-
 app.use("/message", messageRoute);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,9 @@
 require("module-alias/register");
 require("dotenv").config();
 
-const cors = require("cors");
-const app = require("~/app");
 const http = require("http");
+const app = require("~/app");
 const { Server } = require("socket.io");
-
-app.use(
-  cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500", "https://ngogiaquyen.id.vn"],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
 
 const server = http.createServer(app);
 
@@ -24,7 +15,7 @@ const io = new Server(server, {
       "http://127.0.0.1:3000",
       "http://localhost:3000",
       "https://ngogiaquyen.id.vn",
-    ], // Khớp với cấu hình CORS
+    ],
     methods: ["GET", "POST"],
   },
 });
@@ -34,5 +25,5 @@ require("~/config/socket")(io);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log("Server listening on http://localhost:5000");
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
